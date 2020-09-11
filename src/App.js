7a0bc8afbe281d584278bd3b8b982548d55bc16b
chrisgarmon <christophergarmon@gmail.com>
2020-09-11 16:28:59 -0400

    jk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import * as C from "./COMPONENTS";
 import {ThemeProvider} from "theme-ui";
 //import {SignIn} from "./components/Auth";
+//import {
+//	useTransform,
+//	motion,
+//	useMotionValue,
+//	AnimateSharedLayout,
+//	AnimatePresence,
+//} from "framer-motion";
+//import {Page, Color} from "framer";
+//import _ from "lodash";
+
 //import _ from "lodash";
 import {SuspenseWithPerf} from "./FIREBASE";
 import {useFirestore, useUser, useFirestoreDoc} from "reactfire";
 const {Loader} = C;
+function Input({name, ...rest}) {
+	const ref = useRef();
+	const [selected, setSelected] = useState("");
 
+	const getSelection = () =>
+		ref.current.value.substring(
+			ref.current.selectionStart,
+			ref.current.selectionEnd
+		);
+	return (
+		<>
+			{/*<button onClick={() => (ref)}>H</button>*/}
+			<h6>{selected}</h6>
+			<input
+				ref={ref}
+				onSelect={() => setSelected(getSelection())}
+				className="Input"
+				{...rest}
+			/>
+		</>
+	);
+}
 function Dashboard() {
 	const db = useFirestore();
 	const docref = db.doc("views/Dashboard");
@@ -18,6 +49,7 @@ function Dashboard() {
 		<div>
 			<h3>{state.name}</h3>
 			<h6>signed in as {user.email}</h6>
+			<Input />
 		</div>
 	);
 }
